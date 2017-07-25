a9dae6b6de52afb60534de77ac4fc00f943145c8
yusisi <530693409@qq.com>
2017-07-25 16:40:40 +0800

    all

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+历史页面
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import {getRecommend} from 'api/recommend'
+import {ERR_OK} from 'api/config'
+export default {
+    created() {
+        this._getRecommend()
+    },
+    methods: {
+        _getRecommend() {
+            getRecommend().then((res) => {
+                if (res.code === ERR_OK) {
+                    console.log(res.data.slider)
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
