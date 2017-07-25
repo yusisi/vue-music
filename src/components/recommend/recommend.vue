d3b98db81362a7f19faea503427367e24d42db63
yusisi <530693409@qq.com>
2017-07-25 18:35:58 +0800

    a

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-历史页面
+  <div class="recommend">
+      <div class="recommend-content">
+          <div v-if="recommends.length" rel="slideWrapper" class="slider-wrapper">
+              <slider>
+                  <div v-for="item in recommends">
+                        <a :href="item .linkUrl">
+                            <img class="needsclick" :src="item.picUrl" @load="loadImage" alt="">
+                        </a>
+                  </div>
+              </slider>
+          </div>
+          <div class="recommend-list">
+              <h1></h1>
+              <ul></ul>
+          </div>
+      </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Slider from 'base/alider/slider'
 import {getRecommend} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 export default {
+    data() {
+        return {
+            recommends: []
+        }
+    },
     created() {
         this._getRecommend()
     },
@@ -15,7 +35,7 @@ export default {
         _getRecommend() {
             getRecommend().then((res) => {
                 if (res.code === ERR_OK) {
-                    console.log(res.data.slider)
+                    this.recommends = res.data.slider
                 }
             })
         }
