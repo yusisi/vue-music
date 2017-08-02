73122069d9b98844761cce834fa4dd08d556a85c
yusisi <530693409@qq.com>
2017-08-02 18:07:30 +0800

    111

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -61,9 +61,9 @@ export default {
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-.slide-enter-active, .slide-leave-active
-    transition: all 0.3s
+    .slide-enter-active, .slide-leave-active
+        transition: all 0.3s
 
-.slide-enter, .slide-leave-to
-    transform: translate3d(100%, 0, 0)
+    .slide-enter, .slide-leave-to
+        transform: translate3d(100%, 0, 0)
 </style>
