03eb8d369f5385db90eb70c0f1133e93a0d836ad
yusisi <530693409@qq.com>
2017-08-09 17:38:54 +0800

    11

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -6,27 +6,27 @@
 </template>
 
 <script>
-    import ListView from 'base/listview/listview'
-    import {getSingerList} from 'api/singer'
-    import {ERR_OK} from 'api/config'
-    import Singer from 'common/js/singer'
-    import {mapMutations} from 'vuex'
-    import {playlistMixin} from 'common/js/mixin'
+import ListView from 'base/listview/listview'
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+import { mapMutations } from 'vuex'
+import { playlistMixin } from 'common/js/mixin'
 
-    const HOT_SINGER_LEN = 10
-    const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
 
-    export default {
-        mixins: [playlistMixin],
-        data() {
-            return {
-                singers: []
-            }
-        },
-        created() {
-            this._getSingerList()
-        },
-        methods: {
+export default {
+    mixins: [playlistMixin],
+    data() {
+        return {
+            singers: []
+        }
+    },
+    created() {
+        this._getSingerList()
+    },
+    methods: {
         handlePlaylist(playlist) {
             const bottom = playlist.length > 0 ? '60px' : ''
             this.$refs.singer.style.bottom = bottom
@@ -34,68 +34,68 @@
         },
         selectSinger(singer) {
             this.$router.push({
-            path: `/singer/${singer.id}`
+                path: `/singer/${singer.id}`
             })
             this.setSinger(singer)
         },
         _getSingerList() {
             getSingerList().then((res) => {
-            if (res.code === ERR_OK) {
-                this.singers = this._normalizeSinger(res.data.list)
-            }
+                if (res.code === ERR_OK) {
+                    this.singers = this._normalizeSinger(res.data.list)
+                }
             })
         },
         _normalizeSinger(list) {
             let map = {
-            hot: {
-                title: HOT_NAME,
-                items: []
-            }
-            }
-            list.forEach((item, index) => {
-            if (index < HOT_SINGER_LEN) {
-                map.hot.items.push(new Singer({
-                name: item.Fsinger_name,
-                id: item.Fsinger_mid
-                }))
-            }
-            // 
-            const key = item.Findex
-            if (!map[key]) {
-                map[key] = {
-                title: key,
-                items: []
+                hot: {
+                    title: HOT_NAME,
+                    items: []
                 }
             }
-            map[key].items.push(new Singer({
-                name: item.Fsinger_name,
-                id: item.Fsinger_mid
-            }))
+            list.forEach((item, index) => {
+                if (index < HOT_SINGER_LEN) {
+                    map.hot.items.push(new Singer({
+                        name: item.Fsinger_name,
+                        id: item.Fsinger_mid
+                    }))
+                }
+                // 
+                const key = item.Findex
+                if (!map[key]) {
+                    map[key] = {
+                        title: key,
+                        items: []
+                    }
+                }
+                map[key].items.push(new Singer({
+                    name: item.Fsinger_name,
+                    id: item.Fsinger_mid
+                }))
             })
             // 为了得到有序列表，我们需要处理 map
             let ret = []
             let hot = []
             for (let key in map) {
-            let val = map[key]
-            if (val.title.match(/[a-zA-Z]/)) {
-                ret.push(val)
-            } else if (val.title === HOT_NAME) {
-                hot.push(val)
-            }
+                let val = map[key]
+                if (val.title.match(/[a-zA-Z]/)) {
+                    ret.push(val)
+                } else if (val.title === HOT_NAME) {
+                    hot.push(val)
+                }
             }
             ret.sort((a, b) => {
-            return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+                return a.title.charCodeAt(0) - b.title.charCodeAt(0)
             })
             return hot.concat(ret)
         },
         ...mapMutations({
             setSinger: 'SET_SINGER'
         })
-        },
-        components: {
-            ListView
-        }
+    },
+    components: {
+        ListView
     }
+}
 
 </script>
 
